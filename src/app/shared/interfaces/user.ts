f961bb7bc3c9bfbfd15a1cc951444e03906ce2e0
parisk91 <paris_k91@hotmail.com>
2024-04-15 18:28:56 +0300

    14-user-registration

---
+++ src/app/shared/interfaces/user.ts
@@ -0,0 +1,6 @@
+export interface User {
+    givenName: string;
+    surName: string;
+    enail:string;
+    password: string;
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiURL: 'http://coding-factory-api.ddns.net'
+};
