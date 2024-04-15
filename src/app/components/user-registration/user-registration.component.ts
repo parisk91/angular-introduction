a109c48fb5336542d38d4412c1f9b69b9db0c712
parisk91 <paris_k91@hotmail.com>
2024-04-15 20:22:01 +0300

    14

--- src/app/components/user-registration/user-registration.component.ts
+++ src/app/components/user-registration/user-registration.component.ts
@@ -1,4 +1,3 @@
-import { group } from '@angular/animations';
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -7,7 +6,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { User } from 'src/app/shared/interfaces/user';
 import { UserService } from 'src/app/shared/services/user.service';
-
+ 
 @Component({
   selector: 'app-user-registration',
   standalone: true,
@@ -16,42 +15,40 @@ import { UserService } from 'src/app/shared/services/user.service';
   styleUrl: './user-registration.component.css'
 })
 export class UserRegistrationComponent {
-
+ 
   userService = inject(UserService);
-
+ 
   form = new FormGroup({
-    givenName: new FormControl("", Validators.required),
+    givenName: new FormControl('', Validators.required),
     surName: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormGroup('', [Validators.required, Validators.minLength(4)]),
-    confirmPassword: new FormGroup('', [Validators.required, Validators.minLength(4)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(4)]),
   },
-  this.passwordConfirmValidator,
+  this.passwordConfirmsValidator
 );
-
-  passwordConfirmValidator(form: FormGroup) {
-    if (form.get('password').value !== form.get('confirmPassword').value) {
-      form.get('confirmPassword').setErrors({passwordMismatch : true });
-      return {passwordMismatch : true }
-    }
-    return {}
+  passwordConfirmsValidator(form: FormGroup) {
+    if(form.get('password').value !== form.get('confirmPassword').value) {
+      form.get('confirmPassword').setErrors({passwordMismatch: true});
+      return {passwordMismatch: true}
+      }
+      return {}
   }
-
-  onSubmit(value: any) {
-    console.log(value);
-  
-
+onSubmit(value:any) {
+  console.log(value);
+ 
   const user = this.form.value as User
-  delete user['confirmPassword']
-
+  delete user ['confirmPassword']
+ 
   this.userService.registerUser(user).subscribe({
     next: (response) => {
       console.log('User registered', response.msg)
     },
     error: (response) => {
-      const message = response.error.message
-      console.log('Error registering user', message)
+      const message = response.error.msg
+      console.log('Error registrering user', message);
     },
+   
   })
-  }
+}
 }
